615ee1a2e8533309f939255671ef0ccf396aa13b
amruthajagadish7259-max <amruthajagadish7259@gmail.com>
2026-01-06 11:39:44 +0530

    Create test_password.py

---
+++ test_password.py
@@ -0,0 +1,16 @@
+from password import check_password_strength
+
+def test_strong_password():
+    assert check_password_strength("Amrutha123") == "Strong Password"
+
+def test_short_password():
+    assert check_password_strength("Ab12") == "Weak Password"
+
+def test_no_uppercase():
+    assert check_password_strength("amrutha123") == "Weak Password"
+
+def test_no_lowercase():
+    assert check_password_strength("AMRUTHA123") == "Weak Password"
+
+def test_no_digit():
+    assert check_password_strength("Amrutha") == "Weak Password"
